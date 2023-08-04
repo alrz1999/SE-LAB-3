0f39982db2f81308361553aba0b2b320b4c8efcc
Abz81 <101315890+Abz81@users.noreply.github.com>
2023-08-04 18:41:43 +0330

    Update CalculatorStepdefs.java

--- Calculator/src/test/java/calculator/CalculatorStepdefs.java
+++ Calculator/src/test/java/calculator/CalculatorStepdefs.java
@@ -12,17 +12,18 @@ public class CalculatorStepdefs {
     private Calculator.OperationType operationType;
     private int value2;
     private int result;
+    private boolean exceptionThrown;
 
     @Before
     public void before() {
         calculator = new Calculator();
+        exceptionThrown = false;
     }
 
     @Given("Two input values, {int} and {int}. Operator is {word}")
     public void twoInputValuesAndOperatorIs(int arg0, int arg1, String operator) {
         value1 = arg0;
         value2 = arg1;
-
         switch (operator) {
             case "*" -> operationType = Calculator.OperationType.MULTIPLY;
             case "/" -> operationType = Calculator.OperationType.DIVISION;
@@ -33,15 +34,26 @@ public class CalculatorStepdefs {
 
     @When("I perform operator on the two values")
     public void iPerformOperatorOnTheTwoValues() {
-        switch (operationType) {
-            case MULTIPLY -> result = calculator.multiply(value1, value2);
-            case DIVISION -> result = calculator.division(value1, value2);
-            case POWER -> result = calculator.power(value1, value2);
+        try {
+            switch (operationType) {
+                case MULTIPLY -> result = calculator.multiply(value1, value2);
+                case DIVISION -> result = calculator.division(value1, value2);
+                case POWER -> result = calculator.power(value1, value2);
+            }
+        } catch (Exception e) {
+            // Set exceptionThrown to true
+            exceptionThrown = true;
         }
     }
 
     @Then("I expect the result {int}")
     public void iExpectTheResult(int arg0) {
+        // If an exception was thrown, assert it as true
+        if(exceptionThrown){
+        Assert.assertTrue("An exception should have been thrown", exceptionThrown);}
+        // Otherwise, assert the result as expected
+        else{
         Assert.assertEquals(arg0, result);
-    }
+    }}
 }
+
